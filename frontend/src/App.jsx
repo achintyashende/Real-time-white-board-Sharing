eaeca0f864fbce21d3a0b8617dec7023bceabd51
Achintya <achintyashende@gmail.com>
2024-07-23 23:37:52 +0530

    backend added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Forms from './components/Forms'
 
 const App = () => {
 
   return (
     <>
-      <div className='App'>
-        <h1>Hello World</h1>
+      <div className='container'>
+        <Forms/>
       </div>
       
     </>
